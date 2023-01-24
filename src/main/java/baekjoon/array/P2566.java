8d3610a65610f8577be813e92b5ce8d85d204edf
Eundms <dms011221@gmail.com>
2023-01-24 22:12:36 +0900

    array 기본

---
+++ src/main/java/baekjoon/array/P2566.java
@@ -0,0 +1,29 @@
+package baekjoon.array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P2566 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int maxValue=Integer.MIN_VALUE;
+        int maxX=0;
+        int maxY=0;
+        for(int i=0;i<9;i++){
+            st= new StringTokenizer(br.readLine());
+            for(int j=0;j<9;j++){
+                int nowValue=Integer.parseInt(st.nextToken());
+                if(maxValue<nowValue){
+                    maxValue=nowValue;
+                    maxX=i;
+                    maxY=j;
+                }
+            }
+        }
+        System.out.println(maxValue);
+        System.out.println((maxX+1)+" "+(maxY+1));
+    }
+}
---
+++ src/main/java/baekjoon/sort/P2587.java
@@ -0,0 +1,26 @@
+package baekjoon.sort;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class P2587 {
+    public static void main(String[] args) throws IOException {
+        //System.setIn(new FileInputStream("src/main/java/baekjoon/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[5];
+        int allSum=0;
+        for(int i=0;i<5;i++){
+            int now = Integer.parseInt(br.readLine());
+            allSum+=now;
+            array[i]=now;
+        }
+        Arrays.sort(array);
+        System.out.println(allSum/5);
+        System.out.println(array[2]);
+
+    }
+}
