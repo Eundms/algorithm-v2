c6da9865914d0297ab6b8bb1b0a6118756407012
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2023-02-26 14:59:37 +0900

    [Silver II] Title: 부분수열의 합, Time: 208 ms, Memory: 15880 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,57 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int S;
+    static int N, S;
     static int[] arr;
+    static boolean[] visited;
+    static int res;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); // 정수 나타내는 N
+        S = Integer.parseInt(st.nextToken()); // 정수 S
 
-        // 정수의 개수
-        N = Integer.parseInt(st.nextToken());
-        S = Integer.parseInt(st.nextToken());
-
+        arr = new int[N];
         st = new StringTokenizer(br.readLine());
-        arr = new int[N+1];
-        for(int i=1;i<=N;i++){
-            arr[i]=Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        int[] result = new int[N+1];
-        boolean[] visited = new boolean[N+1];
-        int depth=0;
-        for(int n = 1;n<=N;n++)
-           dfs(1,visited,result,depth,n);
-        System.out.println(count);
-    }
-    static int count=0;
-    public  static void dfs(int start, boolean[] visited,int[] result, int depth, int R){
-        if(depth==R){
-            int sum=0;
-            for(int i=0;i<R;i++){
-                sum+=result[i];
-                //System.out.print(result[i]+" ");
-            }
-            if(sum==S){
-                count++;
-            }
-            //System.out.println(" = "+sum);
-            return;
-        }
-        for(int i=start;i<=N;i++){
-            if(!visited[i]){
-                visited[i]=true;
-                result[depth]=arr[i];
-                dfs(i,visited,result,depth+1,R);
-                visited[i]=false;
-            }
-        }
+
+        //합이 S가 되는 부분 수열의 개수?
+        visited = new boolean[N];
+        subset(0);
+        System.out.println(res);
     }
 
+    private static void subset(int cnt) {
+        if (cnt == N) {
+            int cntObj=0;
+            int sum = 0;
+            for (int i = 0; i < N; i++) {
+                if (visited[i]) {
+                    cntObj+=1;
+                    sum += arr[i];
+                }
+            }
+            if (sum == S && cntObj>0) {
+                res +=1;
+            }
+            return ;
+        }
+        visited[cnt] = true;
+        subset(cnt + 1);
 
+        visited[cnt] = false;
+        subset(cnt + 1);
+    }
 }
