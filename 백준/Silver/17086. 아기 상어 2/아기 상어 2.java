8e94573f9320a2e098a50771be041788f6d7a20b
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2023-02-26 13:24:57 +0900

    [Silver II] Title: 아기 상어 2, Time: 456 ms, Memory: 126168 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
@@ -1,100 +1,64 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    /*주의 : 상어끼리의 거리 최솟값 구하는 것이 아니었음*/
-    private static int N;
-    private static int M;
-    private static int[][] box;
-
+    static int N, M;
+    static int[][] box;
+    static boolean[][] visited;
+    static int max = Integer.MIN_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        box = new int[N][M];
+        box = new int[N][M]; // 8방향 거리
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {//0빈칸, 1 아기상어
+            for (int j = 0; j < M; j++) {
                 box[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        int max = Integer.MIN_VALUE;
+        // N x M 크기의 공간에 아기 상어 여러 마리
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {// 0빈칸, 1 아기상어
-                if (box[i][j] != 1) {
-                    int des = bfs(i, j);
-                    max = Math.max(des, max);
+            for (int j = 0; j < M; j++) {
+                if (box[i][j] != 1) { // 아기 상어
+                    visited = new boolean[N][M];
+                    bfs(i, j);
                 }
             }
         }
         System.out.println(max);
 
-
     }
 
+    static int[][] way = new int[][]{{-1, 0}, {1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, -1}, {-1, 1}, {1, -1}};
 
-    private static int bfs(int x, int y) {
-        int[][] visited = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                visited[i][j] = 0;
-            }
-        }
-        int[][] way = {{0, -1}, {1, -1}, {1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}};
-        Queue<Item> queue = new LinkedList<>();
-        queue.add(new Item(x, y, 0));
-        visited[x][y] = 1;
+    private static void bfs(int i, int j) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        visited[i][j] = true;
+        queue.add(new int[]{i, j, 0});
 
         while (!queue.isEmpty()) {
-            Item now = queue.poll();
-
-            for (int w = 0; w < way.length; w++) {
-                int nextx = way[w][0] + now.getx();
-                int nexty = way[w][1] + now.getY();
-                if (nextx >= 0 && nextx < N && nexty >= 0 && nexty < M) {
-                    if (visited[nextx][nexty] == 1) {
-                        continue;
-                    }
-                    if (box[nextx][nexty] == 1) {
-                        return now.getdes()+1;
-                    }
-                    queue.add(new Item(nextx, nexty, now.getdes() + 1));
-                    visited[nextx][nexty] = 1;
+            int[] now = queue.poll();
+            for (int w = 0; w < 8; w++) {
+                int nx = now[0] + way[w][0];
+                int ny = now[1] + way[w][1];
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M || visited[nx][ny]) {
+                    continue;
                 }
+                if (box[nx][ny] == 1) { // 아기 상어
+                    max = Math.max(now[2]+1,max);
+                    return;
+                }
+                visited[nx][ny] = true; // 방문 처리
+                queue.add(new int[]{nx, ny, now[2] + 1});
             }
         }
-        return 0;
-    }
-}
-
-class Item {
-    int x;
-    int y;
-    int des;
-
-    Item(int x, int y, int des) {
-        this.x = x;
-        this.y = y;
-        this.des = des;
-    }
-
-    int getx() {
-        return x;
-    }
-
-    int getY() {
-        return y;
-
-    }
-
-    int getdes() {
-        return des;
     }
 }
