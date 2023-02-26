baf8973fe1102bc338aa3f9ae319d0fb87696acb
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2023-02-26 14:14:39 +0900

    [Silver III] Title: 모든 순열, Time: 1904 ms, Memory: 50804 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.java
+++ 백준/Silver/10974. 모든 순열/모든 순열.java
@@ -5,33 +5,33 @@ import java.io.InputStreamReader;
 public class Main {
     static int N;
     static int[] arr;
+    static boolean[] visited;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        boolean[] visited=new boolean[N+1];
-        int[] result = new int[N+1];
-        arr = new int[N+1];
-        for(int i=1;i<=N;i++){
-            arr[i]=i;
-        }
-        dfs(visited,result,0);
 
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        visited = new boolean[N+1];
+        permutation(0);
     }
-    public  static void dfs(boolean[] visited,int[] result, int depth){
-        if(depth==N){
+
+    private static void permutation(int cnt) {
+        if (cnt == N) {
             for(int i=0;i<N;i++){
-                System.out.print(result[i]+" ");
+                System.out.print(arr[i]+" ");
             }
             System.out.println();
             return;
         }
-        for(int i=1;i<=N;i++){
-            if(!visited[i]){
-                visited[i]=true;
-                result[depth]=arr[i];
-                dfs(visited,result,depth+1);
-                visited[i]=false;
+        for (int i = 1; i <= N; i++) {
+            if (visited[i]) {
+                continue;
             }
+            arr[cnt] = i;
+            visited[i] = true;
+            permutation(cnt + 1);
+            visited[i] = false;
         }
     }
 }
