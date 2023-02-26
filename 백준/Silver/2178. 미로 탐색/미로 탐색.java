cc072337a6dc554d39da6776a2d8d1b51c3d70c4
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2023-02-26 17:43:43 +0900

    [Silver I] Title: 미로 탐색, Time: 136 ms, Memory: 14520 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,17 +1,13 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {// 미로탐색
-    static int result;
-    static int N;
-    static int M;
-    static int[][] miro; // 미로
-    static int[][] way = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+public class Main {
+    static int N, M;
+    static int[][] miro;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,36 +21,35 @@ public class Main {// 미로탐색
                 miro[i][j] = str.charAt(j) - '0';
             }
         }
-
-        bfs(0, 0); // (0,0) -> (N,M)
-
-        System.out.println(result);
+        // bfs
+        bfs(0, 0);//n-1, m-1
     }
 
-    private static void bfs(int i, int j) {
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.add(new int[]{i,j,1});
-        while(!queue.isEmpty()){
-            int[] loc = queue.poll();
-            for (int w = 0; w < 4; w++) {
-                int ni = way[w][0] + loc[0];
-                int nj = way[w][1] + loc[1];
-                int cnt = loc[2] + 1;
+    static int[][] way = new int[][]{{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
 
-                if (ni < 0 || ni >= N || nj < 0 || nj >= M) {
+    private static void bfs(int i, int j) {
+        boolean[][] visited = new boolean[N][M];
+
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[]{i, j, 1});
+        visited[i][j] = true;
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            for (int w = 0; w < 4; w++) {
+                int nx = now[0] + way[w][0];
+                int ny = now[1] + way[w][1];
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M || visited[nx][ny] || miro[nx][ny] == 0) {
                     continue;
                 }
-                if (miro[ni][nj] != 1) {
-                    continue;
-                }
-                if(ni==N-1 && nj == M-1){
-                    result = cnt;
+                if (nx == N - 1 && ny == M - 1) {
+                    System.out.println(now[2] + 1);
                     return;
                 }
-                miro[ni][nj] = cnt; //visited 배열 역할 같이함
-                queue.add(new int[]{ni,nj,cnt});
+                visited[nx][ny] = true;
+                queue.add(new int[]{nx,ny,now[2]+1});
             }
         }
+
     }
 
 }
