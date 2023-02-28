e181e03ef6f09917b9368411b8ab4f0baf8fb79f
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2023-02-28 20:03:05 +0900

    Update update.py

--- update.py
+++ update.py
@@ -3,47 +3,54 @@
 import os
 from urllib import parse
 
-HEADER="""
+HEADER="""# 
 # 백준 & 프로그래머스 & SWEA 문제 풀이 목록
 """
 
 def main():
     content = ""
     content += HEADER
-<<<<<<< HEAD
+    
+    directories = [];
+    solveds = [];
 
-    directories = []
-    solveds = []
-
-    for root, dirs, files in os.walk("."):#\\src\\main\\java
+    for root, dirs, files in os.walk("."):
         dirs.sort()
-        if root == '.':#\\src\\main\\java
-         for dir in ('main', 'java'):
+        if root == '.':
+            for dir in ('.git', '.github'):
                 try:
                     dirs.remove(dir)
                 except ValueError:
                     pass
             continue
+
         category = os.path.basename(root)
-        directory = os.path.basename(os.path.dirname(root))
-
-        if directory == 'java':
+        
+        if category == 'images':
             continue
+        
+        directory = os.path.basename(os.path.dirname(root))
+        
+        if directory == '.':
+            continue
+            
         if directory not in directories:
-            if directory in ["백준", "swexpertacademy","프로그래머스"]:
+            if directory in ["백준", "프로그래머스"]:
                 content += "## 📚 {}\n".format(directory)
+            else:
+                content += "### 🚀 {}\n".format(directory)
+                content += "| 문제번호 | 링크 |\n"
+                content += "| ----- | ----- |\n"
             directories.append(directory)
-        content += "### 🚀 {}\n".format(category)
-        content += "| 문제번호 | 링크 |\n"
-        content += "| ----- | ----- |\n"
+
         for file in files:
-            print(file,solveds)
-            if file not in solveds:
-                content += "|{}|[링크]({})|\n".format(file.split('.')[0], parse.quote(os.path.join(root, file)))
-                solveds.append(file)
+            if category not in solveds:
+                content += "|{}|[링크]({})|\n".format(category, parse.quote(os.path.join(root, file)))
+                solveds.append(category)
+                print("category : " + category)
 
-    with open("README.md", "w",encoding='utf-8') as fd:
+    with open("README.md", "w") as fd:
         fd.write(content)
-
+        
 if __name__ == "__main__":
     main()
