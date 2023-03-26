b8f257d7819aaea52f48c03b26bfd439cbd5ab96
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2023-03-26 11:56:31 +0900

    [Silver I] Title: 회의실 배정, Time: 512 ms, Memory: 43784 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -2,41 +2,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int[][] time;
+    static int N; // 회의의 수
+    static int[][] times; // 시작시간, 끝시간
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine()); // 회의의 수
-
-        time = new int[N][2];
         StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        times = new int[N][2];
         for (int n = 0; n < N; n++) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            time[n][0] = start;
-            time[n][1] = end;
+            times[n][0] = Integer.parseInt(st.nextToken());
+            times[n][1] = Integer.parseInt(st.nextToken());
         }
-        //끝나는 시간으로 정렬, 끝나는 시간이 같다면, 시작 시간으로 정렬
-        Arrays.sort(time,(x,y)->{
-            if(x[1]==y[1]) return Integer.compare(x[0],y[0]);
-            else return Integer.compare(x[1],y[1]);
+
+        Arrays.sort(times, (o1, o2) -> {
+            if (o1[1] == o2[1]) {
+                return o1[0] - o2[0];
+            }
+            return o1[1] - o2[1];
         });
-        
+
         int cnt = 1;
-        int end = time[0][1];
-        for(int n=1;n<N;n++){
-            if (end<=time[n][0]){//끝나는 시각 < 시작하는 시각
-                end = time[n][1];
-                cnt+=1;
+        int end = times[0][1];
+        for (int n = 1; n < N; n++) {
+            if (end <= times[n][0]) { // 끝나는 시각 <= 시작하는 시각 : 해당 회의 진행가능
+                end = times[n][1];
+                cnt += 1;
             }
         }
         System.out.println(cnt);
-
     }
-
 }
