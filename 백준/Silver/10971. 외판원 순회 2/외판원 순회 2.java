9fcc06ab9b22e68165ba1e7e599e280c78e6d581
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2023-03-29 14:07:44 +0900

    [Silver II] Title: 외판원 순회 2, Time: 372 ms, Memory: 14600 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,67 +1,52 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    // 외판원 순회2
-    static boolean[] visited;
-    /**
-     * 단방향 간선 간선의 비용은 양의 정수 가장 적은 비용 원래 자리로 돌아와야 함
-     */
     static int N; // 도시의 수
-    static int[][] adjMatrix; // 비용 행렬
-    static int minDistance = Integer.MAX_VALUE;
-    static int start; //시작 지점
+    static int[][] W;
+    static int start;
+    static boolean[] visited; // 방문 여부
+    static int minCost = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        adjMatrix = new int[N + 1][N + 1]; // i->j 위한 비용
 
+        W = new int[N + 1][N + 1]; // 비용 행렬
         StringTokenizer st;
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= N; j++) {
-                adjMatrix[i][j] = Integer.parseInt(st.nextToken());
+                W[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        // 비용행렬, 순회할 수 있는가
-        for (int i = 1; i <= N; i++) {
-            visited = new boolean[N + 1];
-            visited[i] = true;
-            start = i;
-            dfs(i, 0);
+
+        visited = new boolean[N + 1]; // 1 ~ N번 도시 방문 여부 체크
+        for (start = 1; start <= N; start++) { // 어느 한 도시에서 출발
+            visited[start] = true;
+            back(start, 0, 0);
+            visited[start] = false;
         }
-        System.out.println(minDistance);
+        System.out.println(minCost);
     }
 
-    private static void dfs(int now, int sum) {
-        if (allVisited()) { //모두 방문했다면
-            if (adjMatrix[now][start] != 0) { //다시 시작지점으로 돌아올 수 있다면
-                minDistance = Math.min(sum + adjMatrix[now][start], minDistance);
+    // 상태 변화함 , 변화한 상태 기억해야 함
+    private static void back(int now, int depth, int sum) {
+
+        if (depth == N - 1) { // 기저조건
+            if (W[now][start] != 0) { // 끝 -> 출발지 갈 수 있다면,
+                minCost = Math.min(minCost, sum + W[now][start]);
             }
             return;
         }
 
-        for (int next = 1; next <= N; next++) {
-            if (adjMatrix[now][next] == 0 || visited[next]) {
-                continue;
-            }
-            visited[next] = true;
-            dfs(next, sum + adjMatrix[now][next]);
-            visited[next] = false;
-        }
-    }
-
-    private static boolean allVisited() {
         for (int i = 1; i <= N; i++) {
-            if (!visited[i]) {
-                return false;
-            }
+            if (W[now][i] == 0 || visited[i]) continue;
+            visited[i] = true;
+            back(i, depth + 1, sum + W[now][i]);
+            visited[i] = false;
         }
-        return true;
     }
 }
