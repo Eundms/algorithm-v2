509ba9e9b7d2c9c06e5f7d7a4f5b4326fe352140
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2024-10-05 11:04:43 +0900

    [Silver III] Title: N과 M (6), Time: 116 ms, Memory: 14644 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -1,49 +1,50 @@
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
-
 public class Main {
-    static int N, M; // 자연수 N과 M
-    //static boolean[] visited;
-    static int[] arr;
-    static int[] numbers;
-    static StringBuilder sb = new StringBuilder();
+	static int N, M;
+	static boolean[] visited;
+	static int[] val;
+	static int[] arr;
+	static StringBuilder sb;
+	public static void main(String[] args) throws Exception {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st = new StringTokenizer(br.readLine());
+	    N = Integer.parseInt(st.nextToken()); // 정수의 개수
+	    M = Integer.parseInt(st.nextToken()); // 수열의 원소를 다 더한 값
+	    visited = new boolean[N];
+	    val =  new int[N];
+	    st = new StringTokenizer(br.readLine());
+	    for(int i = 0; i < N; i++) {
+	    	val[i] = Integer.parseInt(st.nextToken());
+	    }
+	    Arrays.sort(val);
+	    arr = new int[M]; 
+	    sb = new StringBuilder();
+	    back(0, 0);
+	    System.out.println(sb);
+	 
+	}
+	static void back(int cnt, int idx) {
+		if(cnt == M) {
+			for(int i = 0; i < M; i++) {
+				sb.append(arr[i]+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = idx; i < N; i++) { // 선택지인덱스
+			if(visited[i])continue;
+			visited[i] = true;
+			arr[cnt] = val[i];
+			back(cnt + 1, i+1);
+			visited[i] = false;
+			
+		}
+		
+	}
 
-    // 1 ~ N 자연수 중 M개 고르기
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        arr = new int[N];
-        st = new StringTokenizer(br.readLine());
-        for (int n = 0; n < N; n++) {
-            arr[n] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(arr);
-        //visited = new boolean[N + 1]; // 방문 1~ N
-        numbers = new int[M];
-        perm(0,0);
-        System.out.print(sb);
-    }
-
-    private static void perm(int cnt,int start ) {
-        if (cnt == M) { // 길이가 M인 수열
-            for (int m = 0; m < M; m++) {
-                sb.append(numbers[m] + " ");
-            }
-            sb.append("\n");
-            return;
-        }
-        for (int i = start; i < N; i++) {
-            //if(visited[i])continue;
-            //visited[i]=true;
-            numbers[cnt] = arr[i];
-            perm(cnt + 1,i+1);
-            //visited[i]=false;
-        }
-    }
 }
