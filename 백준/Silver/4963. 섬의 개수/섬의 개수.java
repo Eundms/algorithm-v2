89768a0dc141f9f13ce69b8fc7976cefa3303092
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2024-11-08 11:57:59 +0900

    [Silver II] Title: 섬의 개수, Time: 144 ms, Memory: 15932 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,68 +1,70 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int w, h;
-    static int[][] box;
-    static boolean[][] visited;
+	static int W, H;
+	static int[][] box;
+	static int LAND = 1, WATER = 0;
+	static boolean[][] visited;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        while (true) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            w = Integer.parseInt(st.nextToken()); // 지도의 너비
-            h = Integer.parseInt(st.nextToken()); // 지도의 높이
-            if (w == 0 && h == 0) {
-                break;
-            }
-            box = new int[h][w]; // 너비, 높이
-            for (int i = 0; i < h; i++) {
-                st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < w; j++) {
-                    box[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
-            int cnt = 0;
-            visited = new boolean[h][w];
-            for (int i = 0; i < h; i++) { // 너비
-                for (int j = 0; j < w; j++) { // 높이
-                    if (box[i][j] == 1 && visited[i][j] == false) {//땅
-                        bfs(i, j);
-                        cnt += 1;
-                    }
-                }
-            }
-            System.out.println(cnt);
-        }
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			W = Integer.parseInt(st.nextToken());
+			H = Integer.parseInt(st.nextToken());
+			if (W == 0 && H == 0) {
+				break;
+			}
+			box = new int[H][W];
+			for (int h = 0; h < H; h++) {
+				st = new StringTokenizer(br.readLine());
+				for (int w = 0; w < W; w++) {
+					box[h][w] = Integer.parseInt(st.nextToken());
+				}
+			}
+			int cnt = 0;
+			visited = new boolean[H][W];
+			for (int i = 0; i < H; i++) {
+				for (int j = 0; j < W; j++) {
 
-    static int[][] way = new int[][]{{1, 0}, {-1, 0}, {0, -1}, {0, 1}, {1, -1}, {-1, 1}, {-1, -1}, {1, 1}};
+					if (box[i][j] == LAND && !visited[i][j]) {
+						bfs(i, j);
+						cnt += 1;
+					}
+				}
+			}
+			System.out.println(cnt);
+		}
 
-    private static void bfs(int i, int j) {
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.add(new int[]{i, j});
-        visited[i][j] = true;
+	}
 
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
+	static int[][] way = new int[][] { { 1, 0 }, { -1, 0 }, { 0, -1 }, { 0, 1 }, { 1, -1 }, { -1, 1 }, { -1, -1 },
+			{ 1, 1 } };
 
-            for (int wi = 0; wi < 8; wi++) {
-                int nx = now[0] + way[wi][0];
-                int ny = now[1] + way[wi][1];
-                if (nx < 0 || nx >= h || ny < 0 || ny >= w || visited[nx][ny]) {
-                    continue;
-                }
-                visited[nx][ny] = true;
-                if (box[nx][ny] == 0) { // 바다인 경우
-                    continue;
-                }
-                queue.add(new int[]{nx, ny});
-            }
-        }
+	static void bfs(int x, int y) {
+		Queue<int[]> queue = new ArrayDeque<>();
+		queue.add(new int[] { x, y });
+		visited[x][y] = true;
 
-    }
+		while (!queue.isEmpty()) {
+			int[] now = queue.poll();
+
+			for (int w = 0; w < 8; w++) {
+				int nx = way[w][0] + now[0];
+				int ny = way[w][1] + now[1];
+				if (nx < 0 || nx >= H || ny < 0 || ny >= W || visited[nx][ny])
+					continue;
+				visited[nx][ny] = true;
+				if (box[nx][ny] == LAND) {
+					queue.add(new int[] { nx, ny });
+				}
+			}
+
+		}
+	}
+
 }
