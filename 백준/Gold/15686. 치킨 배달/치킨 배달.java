b93a11ec490f363c58e111557adbeb5bf948e10c
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2024-11-13 16:43:23 +0900

    [Gold V] Title: 치킨 배달, Time: 2884 ms, Memory: 300364 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,109 +1,103 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Queue;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
-    static int[][] dosi; // 도시
-    static boolean[][] visited; // 도시 방문 여부
-    static List<int[]> chicken; // 치킨 가게
-    static List<int[]> house; // 집
-    static boolean[] cVisited; // 치킨집 선택 여부(총 true : M개)
+	static int N, M;
+	static int[][] box;
+	static int HOME = 1, CHICKEN = 2;
+	static int[] selected;
+	static Set<Integer> selectedSet;
+	static List<int[]> chickens;
+	static int[][] chickPosToId;
+	static int ans = Integer.MAX_VALUE;
 
-    static int[][] choiceDosi;
-    static int minChickenDistance = Integer.MAX_VALUE;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); // N x N 도시
-        M = Integer.parseInt(st.nextToken()); // 치킨집을 몇 개 살릴것인가
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-        chicken = new ArrayList<>();
-        house = new ArrayList<>();
-        dosi = new int[N][N];
-        // N개의 줄 : 도시의 정보
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                dosi[i][j] = Integer.parseInt(st.nextToken()); // 도시의 정보 0(빈칸) 1(집) 2(치킨집)
-                if (dosi[i][j] == 2) { // 치킨집
-                    chicken.add(new int[]{i, j});
-                } else if (dosi[i][j] == 1) { // 집
-                    house.add(new int[]{i, j});
-                }
-            }
-        }
+		chickens = new ArrayList<>();
+		box = new int[N][N];
+		chickPosToId = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				box[i][j] = Integer.parseInt(st.nextToken());
+				chickPosToId[i][j] = Integer.MAX_VALUE;
+				if (box[i][j] == CHICKEN) {
+					chickens.add(new int[] { i, j });
+					chickPosToId[i][j] = chickens.size() - 1;
+				}
+			}
+		}
 
-        // 폐업시킬 [chicken.size()-M]개의 치킨집을 뽑고,
-        cVisited = new boolean[chicken.size()];
-        selectMChicken(0, 0);
-        System.out.println(minChickenDistance);
+		selected = new int[M]; // 치킨집 개수
+		back(0, 0);
+		System.out.println(ans);
+	}
 
-    }
-    private static int[][] simulateDelChicken(){
-       int[][] temp = new int[N][N];
-       for(int i=0;i<N;i++){
-           for(int j=0;j<N;j++){
-                temp[i][j] = dosi[i][j];
-           }
-       }
-       for(int i=0;i<chicken.size();i++){
-           if(cVisited[i]){
-               int[] loc = chicken.get(i);
-               temp[loc[0]][loc[1]] = 0;
-           }
-       }
-       return temp;
-    }
-    private static void selectMChicken(int cnt, int start) {
-        if (cnt == chicken.size()-M) {
-            int allDist = 0;// 해당 경우의 치킨 거리를 구한다.
-            choiceDosi = simulateDelChicken();
-            for (int[] hLoc : house) {
-                visited = new boolean[N][N];
-                allDist += bfs(hLoc[0], hLoc[1], 0);
-            }
-            minChickenDistance = Math.min(allDist,minChickenDistance); // 치킨 거리의 최솟값인지 확인하기
-            return;
-        }
-        for(int i = start;i<chicken.size();i++){
-            if(cVisited[i])continue;
-            cVisited[i]=true;
-            selectMChicken(cnt+1,i+1);
-            cVisited[i] =false;
+	static void back(int cnt, int idx) {
+		if (cnt == M) {
+			selectedSet = new HashSet<>();
+			for (int i = 0; i < M; i++) {
+				selectedSet.add(selected[i]);
+			}
+			int val = 0;
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (box[i][j] == HOME) {
+						int dist = near(i, j);
+						val += dist;
+					}
+				}
+			}
+			ans = Math.min(ans, val);
+			return;
+		}
+		for (int i = idx; i < chickens.size(); i++) {
+			selected[cnt] = i;
+			back(cnt + 1, i + 1);
+		}
+	}
 
-        }
-    }
+	static int[][] way = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 
-    private static int bfs(int i, int j, int cnt) { // 출발지는 집
-        int[][] way = new int[][]{{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.add(new int[]{i, j, cnt});
-        visited[i][j] = true;
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-            for (int w = 0; w < 4; w++) {
-                int nextX = now[0] + way[w][0];
-                int nextY = now[1] + way[w][1];
-                int nextCnt = now[2] + 1;
-                if (nextX < 0 || nextX >= N || nextY < 0 || nextY >= N) {
-                    continue;
-                }
-                if (visited[nextX][nextY]) {
-                    continue;
-                }
-                if (choiceDosi[nextX][nextY] == 2) { // 치킨 집 발견!!
-                    return nextCnt;
-                }
-                visited[nextX][nextY] = true;
-                queue.add(new int[]{nextX, nextY, nextCnt});
-            }
-        }
-        return 0;
-    }
+	static int near(int x, int y) {
+		boolean[][] visited = new boolean[N][N];
+		Queue<int[]> queue = new ArrayDeque<>();
+		visited[x][y] = true;
+		queue.add(new int[] { x, y, 0 });
+
+		while (!queue.isEmpty()) {
+			int[] now = queue.poll();
+			if (box[now[0]][now[1]] == CHICKEN) {
+				if (selectedSet.contains(chickPosToId[now[0]][now[1]])) {
+					return now[2];
+				}
+			}
+			for (int w = 0; w < 4; w++) {
+				int nx = way[w][0] + now[0];
+				int ny = way[w][1] + now[1];
+				if (nx < 0 || nx >= N || ny < 0 || ny >= N || visited[nx][ny])
+					continue;
+				if (box[nx][ny] == CHICKEN && selectedSet.contains(chickPosToId[nx][ny])) {
+					return now[2] + 1;
+				}
+				visited[nx][ny] = true;
+				queue.add(new int[] { nx, ny, now[2] + 1 });
+
+			}
+		}
+		return 0;
+	}
+
 }
