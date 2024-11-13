95db081a3318a2adaa1c3ff211d563a6837d18ae
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2024-11-13 12:21:40 +0900

    [Gold V] Title: 꿀 따기, Time: 276 ms, Memory: 25648 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/21758. 꿀 따기/꿀 따기.java
+++ 백준/Gold/21758. 꿀 따기/꿀 따기.java
@@ -18,61 +18,20 @@ public class Main {
 		}
 
 		int[] left = new int[N];
-		for (int i = 0; i < N; i++) {
-			if (i == 0) {
-				left[i] = box[i];
-			} else {
-				left[i] = box[i] + left[i - 1];
-			}
-		}
-
 		int[] right = new int[N];
-		for (int i = N - 1; i >= 0; i--) {
-			if (i == N - 1) {
-				right[i] = box[i];
-			} else {
-				right[i] = box[i] + right[i + 1];
-			}
+		left[0] = box[0];
+		right[N - 1] = box[N - 1];
+		for (int i = 1; i < N; i++) {
+			left[i] = left[i - 1] + box[i];
+			right[N - 1 - i] = right[N - i] + box[N - 1 - i];
 		}
+
 		int max = 0;
-		for (int h = 0; h < N; h++) { // honey
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					if (h == i || h == j || i == j)
-						continue;
-					int sum = 0;
-					if (h < i) { // h i
-						sum += right[h] - right[i];
-					} else { // i h
-						sum += left[h] - left[i];
-					}
-
-					if (h < j) { // h j
-						sum += right[h] - right[j];
-					} else { // j h
-						sum += left[h] - left[j];
-					}
-
-					if (h < i && h < j) {
-						if (i < j) { // h < i < j
-							sum -= box[i];
-						} else {
-							sum -= box[j];
-
-						}
-					}
-					if (h > i && h > j) {
-						if (i < j) { // h < i < j
-							sum -= box[j];
-						} else {
-							sum -= box[i];
-						}
-					}
-					max = Math.max(max, sum);
-				}
-			}
+		for (int i = 1; i < N - 1; i++) {
+			max = Math.max(max, left[i] - left[0] + right[i] - right[N - 1]);
+			max = Math.max(max, left[N - 1] + left[N - 1] - left[0] - box[i] - left[i]);
+			max = Math.max(max, right[0] + right[0] - right[N - 1] - box[N - 1 - i] - right[N - 1 - i]);
 		}
-
 		System.out.println(max);
 
 	}
