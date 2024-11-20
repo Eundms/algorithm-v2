891681e4cf80cd8eb5619100ddf887863136ba86
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2024-11-20 09:25:23 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 664 ms, Memory: 114004 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,85 +1,71 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main { //https://anott.tistory.com/54
-    static int N, M, box[][];
-    static int[][] way = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    static boolean[][][] visited;
-    static int WALL = 1, EMPTY = 0;
-    static int NOT_CRUSH = 0, CRUSHED = 1;
+public class Main {
+	static int T;
+	static int N, M; // N x N, 하나의 집 비용 M
+	static int[][] box; // 집이 있는 위치 1 , 나머지 0
+	static int EMPTY = 0, WALL = 1;
+	static int answer = Integer.MAX_VALUE;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        box = new int[N][M];
-        for (int n = 0; n < N; n++) {
-            String str = br.readLine();
-            for (int m = 0; m < M; m++) {
-                box[n][m] = str.charAt(m) - '0';
-            }
-        }
-        visited = new boolean[N][M][2]; // n,m까지 오는데 crush해서 방문한경우, crush하지 않고 방문한 경우
-        System.out.println(bfs(0, 0, 0));
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		box = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			String line = br.readLine();
+			for (int j = 0; j < M; j++) {
+				box[i][j] = line.charAt(j) - '0';
+			}
+		}
+		bfs(0, 0, 0);
+		if (answer == Integer.MAX_VALUE) {
+			System.out.println(-1);
+		} else {
+			System.out.println(answer);
 
-    private static int bfs(int x, int y, int crush) {// 1: CRUSH, 0: NO_CRUSH
-        Queue<Position> queue = new ArrayDeque<>();
-        queue.add(new Position(x, y, 0, crush));
-        visited[x][y][crush] = true;
+		}
+	}
 
-        while (!queue.isEmpty()) {
-            Position now = queue.poll();
+	static int[][] way = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 
-            if (now.x == N - 1 && now.y == M - 1) { // 종료조건
-                return now.cnt + 1;
-            }
+	static void bfs(int sx, int sy, int crush) { // 벽 부셔도 됨 한개까지
+		boolean[][][] visited = new boolean[N][M][2];
+		Queue<int[]> queue = new ArrayDeque<>();
+		queue.add(new int[] { sx, sy, 1, 0 }); // 최단거리, 부신 벽 개수
 
-            for (int w = 0; w < 4; w++) {// 벽 부순 상태 1 , 벽 부수지 않은 상태 0
-                int nx = now.x + way[w][0];
-                int ny = now.y + way[w][1];
-                int nCnt = now.cnt + 1;
-                int nWall = now.crush;
-                if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
-                    continue;
-                }
-                if (visited[nx][ny][nWall]) {
-                    continue;
-                }
-
-                // 1. 이동할 수 있는 곳인 경우
-                if (box[nx][ny] == EMPTY) {
-                    visited[nx][ny][nWall] = true;
-                    queue.add(new Position(nx, ny, nCnt, nWall));
-                }
-                // 2. 벽인 경우
-                else if (box[nx][ny] == WALL) {
-                    if (nWall == 0) { // 벽 부순적이 없어야 한다.
-                        visited[nx][ny][crush] = true;
-                        queue.add(new Position(nx, ny, nCnt, CRUSHED)); // 벽 부수고 이동
-                    }
-                }
-
-            }
-        }
-        return -1;
-    }
-
-    static class Position {
-        int x, y, cnt, crush; // 현재 위치까지 거리, 현재 위치까지 벽을 부순경우(1) , 안부순경우(0)
-
-        public Position(int x, int y, int cnt, int crush) {
-            this.x = x;
-            this.y = y;
-            this.cnt = cnt;
-            this.crush = crush;
-        }
-    }
+		while (!queue.isEmpty()) {
+			int[] now = queue.poll();
+			if (now[0] == N - 1 && now[1] == M - 1) {
+				answer = Math.min(now[2], answer);
+			}
+			for (int w = 0; w < 4; w++) {
+				int nx = now[0] + way[w][0];
+				int ny = now[1] + way[w][1];
+				if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+					continue;
+				if (visited[nx][ny][now[3]]) {
+					continue;
+				}
+				// 이동 가능
+				if (box[nx][ny] == EMPTY) {
+					visited[nx][ny][now[3]] = true;
+					queue.add(new int[] { nx, ny, now[2] + 1, now[3] });
+				} else if (box[nx][ny] == WALL) {
+					// 이동 불가능
+					if (now[3] == 0) { // 한번도 벽을 부순적 없음
+						// 벽 부수고 이동
+						visited[nx][ny][1] = true;
+						queue.add(new int[] { nx, ny, now[2] + 1, 1 });
+					}
+				}
+			}
+		}
+	}
 
 }
