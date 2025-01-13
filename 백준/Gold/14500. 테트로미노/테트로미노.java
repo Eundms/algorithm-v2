4ddeeb7ed46126404b2a11be3dacdd5c6fda8a40
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2025-01-13 20:04:31 +0900

    [Gold IV] Title: 테트로미노, Time: 604 ms, Memory: 36476 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -4,79 +4,79 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
-    static int[][] box;
-    static boolean[][] visited;
-    static int max;
+	static int N, M; // 행의 개수, 열의 개수
+	static int[][] box;
+	static int[][] way = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
+	static int max;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        box = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                box[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        visited = new boolean[N][M];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-        //dfs depth=4
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                visited[i][j] = true;
-                dfs(i, j, 0, 0);
+		box = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				box[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		max = Integer.MIN_VALUE;
+		boolean[][] visited = new boolean[N][M];
 
-                visited[i][j] = false;
-                other(i,j);
-            }
-        }
-        System.out.println(max);
-    }
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				visited[i][j] = true;
+				dfs(i, j, box[i][j], 1, visited);
+				visited[i][j] = false;
+				// ㅗ ㅜ ㅓ ㅏ 확인
+				other(i, j);
+			}
+		}
+		System.out.println(max);
 
-    private static void other(int i, int j) {
-        // (0,0), (1,0), (2,0), (1,1) ㅏ
-        if (i >= 0 && i + 2 < N && j >= 0 && j + 1 < M) {
-            max = Math.max(max, box[i][j] + box[i + 1][j] + box[i + 2][j] + box[i + 1][j + 1]);
-        }
+	}
 
-        // (0,0), (0,1), (0,2), (1,1) ㅜ
-        // box[i][j] + box[i][j+1] + box[i][j+2] + box[i+1][j+1]
-        if (i >= 0 && i + 1 < N && j >= 0 && j + 2 < M) {
-            max = Math.max(max, box[i][j] + box[i][j + 1] + box[i][j + 2] + box[i + 1][j + 1]);
-        }
-        // (0,0) (0,1) (-1,1) (1,1) ㅓ
-        // box[i][j] + box[i][j+1] + box[i-1][j+1] + box[i+1][j+1]
-        if (i - 1 >= 0 && i + 1 < N && j >= 0 && j + 1 < M) {
-            max = Math.max(max, box[i][j] + box[i][j + 1] + box[i - 1][j + 1] + box[i + 1][j + 1]);
-        }
-        // (0,0) (0,1) (0,2) (-1,1) ㅗ
-        // box[i][j] + box[i][j+1] + box[i][j+2] + box[i-1][j+1]
-        if (i - 1 >= 0 && i < N && j >= 0 && j + 2 < M) {
-            max = Math.max(max, box[i][j] + box[i][j + 1] + box[i][j + 2] + box[i - 1][j + 1]);
-        }
+	static void other(int i, int j) {
+		for (int s = 0; s < 4; s++) {
+			int sum = box[i][j];
+			for (int l = 0; l < 3; l++) {
+				int idx = (s + l) % 4;
+				int nx = i + way[idx][0];
+				int ny = j + way[idx][1];
+				if (!isValid(nx, ny)) {
+					sum = 0;
+					break;
+				}
+				sum += box[nx][ny];
+			}
+			max = Math.max(sum, max);
+		}
+	}
 
-    }
+	static void dfs(int i, int j, int sum, int depth, boolean[][] visited) {
+		if (depth == 4) {
+			max = Math.max(sum, max);
+			return;
+		}
+		for (int w = 0; w < 4; w++) {
+			int nx = i + way[w][0];
+			int ny = j + way[w][1];
+			if (!isValid(nx, ny)) {
+				continue;
+			}
+			if (visited[nx][ny]) {
+				continue;
+			}
+			visited[nx][ny] = true;
+			dfs(nx, ny, sum + box[nx][ny], depth + 1, visited);
+			visited[nx][ny] = false;
+		}
+	}
 
-    private static void dfs(int i, int j, int depth, int sum) {
-        if (depth == 4) {
-            max = Math.max(max, sum);
-            return;
-        }
-        int[][] way = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-        for (int w = 0; w < 4; w++) {
-            int nx = i + way[w][0];
-            int ny = j + way[w][1];
-            if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
-                continue;
-            }
-            if (!visited[nx][ny]) {
-                visited[nx][ny] = true;
-                dfs(nx, ny, depth + 1, sum + box[nx][ny]);
-                visited[nx][ny] = false;
-            }
-        }
-    }
+	static boolean isValid(int x, int y) {
+		return x >= 0 && x < N && y >= 0 && y < M;
+	}
+
 }
