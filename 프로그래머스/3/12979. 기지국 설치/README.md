e12ff07f154264727473b940ef004de43a060620
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2025-01-16 21:03:19 +0900

    [level 3] Title: 기지국 설치, Time: 0.88 ms, Memory: 54.6 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/README.md
+++ 프로그래머스/3/12979. 기지국 설치/README.md
@@ -1,6 +1,6 @@
 # [level 3] 기지국 설치 - 12979 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12979#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12979) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 16일 21:02:55
+2025년 01월 16일 21:03:17
 
 ### 문제 설명
 
--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -6,27 +6,55 @@ class Solution {
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
         
-        int apt = 1;
+        int sIdx = 0;
         // 시작 끝 미리 정해두기
         // 너비 2 * w + 1 
+        List<Node> nodes = new ArrayList<>();
         for(int i = 0; i < stations.length; i++) {
-            int start = stations[i] - w;
-            int end = stations[i] + w;
-            if (apt < start) {
-                answer += build(start - apt, w);
-            }  
-            apt = end + 1;
+            int start = Math.max(1, stations[i] - w);
+            int end = Math.min(stations[i] + w, n);
+            nodes.add(new Node(start, end));
         }
         
-        if(apt <= n) {
-            answer += build(n-apt+1, w);
+        Collections.sort(nodes);
+        
+        System.out.println(nodes);
+        int apt = 1;
+        for(int i = 0; i < nodes.size(); i++) {
+            Node node = nodes.get(i);
+            if(node.start > apt){
+              // 전파 세워야함
+                int dist = node.start - apt;
+                int buildCnt = build(dist, w);
+                answer += buildCnt;
+                apt = node.end;
+            } 
+        }
+        if(apt < n) {
+            answer += build(n - apt, w);
         }
         
- 
         return answer;
     }
     static int build(int dist, int w) {
         return dist / (2 * w + 1) + (dist % (2 * w + 1) > 0 ? 1 : 0);
     }
-
+    static class Node implements Comparable<Node>{
+        int start, end;
+        Node(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+        @Override
+        public int compareTo(Node o) {
+            if(this.start == o.start) {
+                return this.end - o.end;
+            }
+            return this.start - o.start;
+        }
+        @Override
+        public String toString() {
+            return ""+this.start+" "+this.end;
+        }
+    }
 }
