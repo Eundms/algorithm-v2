abddc4e5bd6c0a4eddfeba1db286354b776adf84
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2025-01-21 14:02:05 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 236 ms, Memory: 26008 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -2,208 +2,115 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
-public class Main { // 미세먼지 안녕! ~
-    static int R, C, T;
-    static int[][] box;
-    static List<int[]> air; // 공기 청정기 설치된 곳
-    static Queue<int[]> mise;// 미세먼지 양
+public class Main {
+	static int R, C, T;
+	static int[][] box;
+	static int[][] way = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        T = Integer.parseInt(st.nextToken()); // T초 지남
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		T = Integer.parseInt(st.nextToken());
+		box = new int[R][C];
+		int bottomR = 0;
+		for (int r = 0; r < R; r++) {
+			st = new StringTokenizer(br.readLine());
+			for (int c = 0; c < C; c++) {
+				box[r][c] = Integer.parseInt(st.nextToken());
+				if (box[r][c] == -1) {
+					bottomR = r;
+				}
+			}
+		}
 
-        air = new ArrayList<>();
+		while (T-- > 0) {
+			spread();
+			filter(bottomR - 2, 0, bottomR - 1, way);
+			filter(bottomR + 1, bottomR, R - 1, new int[][] { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } });
 
-        box = new int[R][C]; // -1 : 공기청정기
-        for (int r = 0; r < R; r++) {
-            st = new StringTokenizer(br.readLine());
-            for (int c = 0; c < C; c++) {
-                box[r][c] = Integer.parseInt(st.nextToken());
-                if (box[r][c] == -1) { // 공기청정기가 설치된 곳
-                    if (box[r - 1][c] == -1) {
-                        air.add(new int[]{r, c});
-                        //System.out.print("r,c "+r+","+c);
-                    }
-                }
-            }
-        }
+		}
 
+		int ans = 0;
+		for (int r = 0; r < R; r++) {
+			for (int c = 0; c < C; c++) {
+				if (box[r][c] != -1) {
+					ans += box[r][c];
+				}
+			}
+		}
+		System.out.println(ans);
 
-        while (T > 0) {
-            mise = new ArrayDeque<>();
-            for (int r = 0; r < R; r++) {
-                for (int c = 0; c < C; c++) {
-                    mise.add(new int[]{r,c});
-                }
-            }
-            // 1. 미세먼지 확산
-            // 모든 칸에서 동시에 일어남
-            temp = new int[R][C];
-            for (int[] item : mise) {
-                miseDiffuse(item[0], item[1]);
-            }
-            box = addMatrix(temp, box);
+	}
 
-            // 2. 공기청정기 작동
-            for (int[] item : air) {
-                airRefresh(item[0], item[1]);
-            }
-            T--;
-        }
+	static void print() {
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
+				System.out.print(box[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println("==========");
+	}
 
-        int res=0;
-        for(int r=0;r<R;r++){
-            for(int c=0;c<C;c++){
-                if(box[r][c]>0){
-                    res+=box[r][c];
-                }
-            }
-        }
-        System.out.println(res);
-    }
+	static void filter(int row, int minRow, int maxRow, int[][] way) {
+		int x = row, y = 0;
 
-    private static int[][] addMatrix(int[][] temp, int[][] box) {
-        for (int r = 0; r < R; r++) {
-            for (int c = 0; c < C; c++) {
-                box[r][c] += temp[r][c];
-            }
-        }
-        return box;
-    }
+		int last = box[x][y];
 
-    static int[][] temp;
+		for (int w = 0; w < 4; w++) {
 
-    private static void miseDiffuse(int x, int y) { // 1. 미세먼지 확산 시작점
+			while (true) {
+				int nx = x + way[w][0];
+				int ny = y + way[w][1];
+				if (nx < 0 || nx >= R || ny < 0 || ny >= C)
+					break;
+				if (!(nx >= minRow && nx <= maxRow)) {
+					break;
+				}
+				if (box[nx][ny] == -1) {
+					box[x][y] = 0;
+				} else {
+					box[x][y] = box[nx][ny];
 
-        int amount = box[x][y] / 5;
-        int cnt = 0;
-        for (int w = 0; w < 4; w++) {
-            int nx = x + clockWay[w][0];
-            int ny = y + clockWay[w][1];
-            // 미세먼지 (r, c) : 인접한 네 방향으로 확산
-            if (nx < 0 || nx >= R || ny < 0 || ny >= C || box[nx][ny] == -1) {// 인접한 방향에 공기청정기가 있다면
-                continue;
-            }
-            // 확산되는 양(amount)은 "A(r,c)/5" 이고 소수점은 버린다.
-            temp[nx][ny] += amount;
-            cnt++;
-        }
-        box[x][y] -= amount * cnt;
-    }
+				}
+				// System.out.println(nx + "," + ny);
+				x = nx;
+				y = ny;
+			}
+		}
+	}
 
-    static int[][] clockWay = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // 시계방향
-    static int[][] rClockWay = new int[][]{{0, 1}, {-1, 0}, {0, -1}, {1, 0}}; // 반시계방향
+	static void spread() {
+		int[][] next = new int[R][C];
+		for (int r = 0; r < R; r++) {
+			for (int c = 0; c < C; c++) {
+				if (box[r][c] > 0) {
+					int dust = box[r][c];
+					int nearDust = dust / 5;
+					int spreadCnt = 0;
+					for (int w = 0; w < 4; w++) {
+						int nx = r + way[w][0];
+						int ny = c + way[w][1];
+						if (inRange(nx, ny) && box[nx][ny] != -1) {
+							spreadCnt += 1;
+							next[nx][ny] += nearDust;
+						}
+					}
+					next[r][c] += dust - nearDust * spreadCnt;
+				} else if (box[r][c] == -1) {
+					next[r][c] = -1;
+				}
+			}
+		}
+		box = next;
+	}
 
-    private static void airRefresh(int x, int y) { // 2. 공기청정기 바람 : 시계방향 순환 -> 아래쪽만 줌..!
+	static boolean inRange(int x, int y) {
+		return x >= 0 && x < R && y >= 0 && y < C;
+	}
 
-        // 위쪽 -> 반시계 ////////////////////////////////////
-        int upX = Math.max(0, x - 1);
-        int upY = y + 1;
-        int rw = 0;
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.add(box[upX][upY]);
-        while (true) {
-            // 다음 방향 확인
-            int nx = upX + rClockWay[rw][0];
-            int ny = upY + rClockWay[rw][1];
-            if (nx < 0 || nx >= R || ny < 0 || ny >= C) {
-                rw += 1;
-                continue;
-            }
-            queue.add(box[nx][ny]);
-
-            upX = nx;
-            upY = ny;
-
-            if (upX == Math.max(0, x - 2) && upY == y) {
-                break;
-            }
-        }
-        box[Math.max(0, x - 1)][y + 1] = 0;
-
-        upX = Math.max(0, x - 1);
-        upY = Math.min(y + 1, C - 1);
-        rw = 0;
-        while (true) {
-            // 다음 방향 확인
-            int nx = upX + rClockWay[rw][0];
-            int ny = upY + rClockWay[rw][1];
-            if (nx < 0 || nx >= R || ny < 0 || ny >= C) {
-                rw += 1;
-                continue;
-            }
-            box[nx][ny] = queue.poll();
-
-            upX = nx;
-            upY = ny;
-
-            if (upX == Math.max(0, x - 2) && upY == y) {
-                break;
-            }
-        }
-
-
-        // 아래쪽 -> 시계 ////////////////////////////////////
-        int downX = x;
-        int downY = y + 1;
-        int w = 0;
-        Queue<Integer> queue2 = new ArrayDeque<>();
-        queue2.add(box[downX][downY]);
-        while (true) {
-            // 다음 방향 확인
-            int nx = downX + clockWay[w][0];
-            int ny = downY + clockWay[w][1];
-            if (nx < 0 || nx >= R || ny < 0 || ny >= C) {
-                w += 1;
-                continue;
-            }
-            queue2.add(box[nx][ny]);
-
-            downX = nx;
-            downY = ny;
-            if (downX == x + 1 && downY == y) {
-                break;
-            }
-        }
-
-        box[x][y + 1] = 0;
-        downX = x;
-        downY = y + 1;
-        w = 0;
-        while (true) {
-            int nx = downX + clockWay[w][0];
-            int ny = downY + clockWay[w][1];
-            if (nx < 0 || nx >= R || ny < 0 || ny >= C) {
-                w += 1;
-                continue;
-            }
-            box[nx][ny] = queue2.poll();
-            downX = nx;
-            downY = ny;
-            if (downX == x + 1 && downY == y) {
-                break;
-            }
-        }
-
-    }
-
-    private static void printMatrix(String str) {
-        System.out.println("==================" + str);
-        for (int i = 0; i < R; i++) {
-            for (int j = 0; j < C; j++) {
-                if (box[i][j] == -1) {
-                    System.out.print("X" + "\t");
-                } else {
-                    System.out.print(box[i][j] + "\t");
-                }
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
 }
