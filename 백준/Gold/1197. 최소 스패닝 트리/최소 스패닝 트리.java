01e83501a3c0a0276642436287fe3791451c93f7
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2025-01-23 10:30:52 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 488 ms, Memory: 57048 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,92 +1,85 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-//크루스칼 => 간선
-// prim => 노드
 public class Main {
-    static int V, E; //정점, 간선
-    static List<Edge> edgeList;
-    static int[] parent;
-    static int[] depth;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        V = Integer.parseInt(st.nextToken()); // 정점의 개수 10,000
-        E = Integer.parseInt(st.nextToken()); // 간선의 개수
-        edgeList = new ArrayList<>();
-        for (int e = 0; e < E; e++) {
-            st = new StringTokenizer(br.readLine());
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            int C = Integer.parseInt(st.nextToken());
-            edgeList.add(new Edge(A, B, C));
-        }
+	static int V, E; // 정점의 개수, 간선의 개수
+	static List<Node>[] adj;
 
-        Collections.sort(edgeList);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
 
-        parent = new int[V + 1]; //1 ~V번 부모
-        for (int i = 0; i < V + 1; i++) {
-            parent[i] = i;
-        }
-        depth = new int[V + 1]; // 깊이
+		adj = new ArrayList[V + 1];
+		for (int i = 0; i <= V; i++) {
+			adj[i] = new ArrayList<>();
+		}
 
-        int mstEdgeNum = V - 1;
-        int sumOfWeight = 0;
-        for (int e = 0; e < E; e++) {
-            if (union(edgeList.get(e).a, edgeList.get(e).b)) {
-                sumOfWeight += edgeList.get(e).weight;
-                mstEdgeNum--;
-            }
-            if (mstEdgeNum == 0) {
-                break;
-            }
-        }
-        System.out.println(sumOfWeight);
+		for (int e = 0; e < E; e++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			adj[a].add(new Node(b, c));
+			adj[b].add(new Node(a, c));
+		}
 
-    }
+		System.out.println(prim());
+	}
 
-    private static boolean union(int a, int b) {
-        a = find(a);
-        b = find(b);
-        if (a == b) {
-            return false;
-        }
-        if (depth[a] < depth[b]) {
-            parent[a] = b;
-        } else {
-            parent[b] = a;
-        }
-        if (depth[a] == depth[b]) {
-            depth[a]++;
-        }
-        return true;
-    }
+	static int prim() {
+		int[] dist = new int[V + 1]; // 각 정점까지의 최소 비용
+		boolean[] visited = new boolean[V + 1]; // 방문 여부
+		Arrays.fill(dist, Integer.MAX_VALUE);
 
-    private static int find(int x) {
-        if (parent[x] == x) {
-            return x;
-        }
-        return parent[x] = find(parent[x]);
-    }
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.add(new Node(1, 0)); // 시작 정점 (1번 정점)
+		dist[1] = 0;
 
-    static class Edge implements Comparable<Edge> {
-        int a, b, weight;
+		int cost = 0;
 
-        public Edge(int a, int b, int weight) {
-            this.a = a;
-            this.b = b;
-            this.weight = weight;
-        }
+		while (!pq.isEmpty()) {
+			Node now = pq.poll();
 
-        @Override
-        public int compareTo(Edge o) {
-            return this.weight - o.weight;
-        }
-    }
+			// 이미 MST에 포함된 정점은 스킵
+			if (visited[now.to])
+				continue;
+
+			visited[now.to] = true; // 현재 정점 방문
+			cost += now.cost;
+
+			// 인접한 정점들 갱신
+			for (Node next : adj[now.to]) {
+				if (!visited[next.to] && dist[next.to] > next.cost) {
+					dist[next.to] = next.cost;
+					pq.add(new Node(next.to, next.cost));
+				}
+			}
+		}
+
+		return cost; // MST의 총 비용 반환
+	}
+
+	static class Node implements Comparable<Node> {
+		int to, cost;
+
+		Node(int to, int cost) {
+			this.to = to;
+			this.cost = cost;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return this.cost - o.cost;
+		}
+	}
 }
