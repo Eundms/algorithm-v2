07dcf4d398711b20787327a044a562edd5c24108
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2025-02-06 19:56:13 +0900

    [Gold III] Title: 내리막 길, Time: 328 ms, Memory: 36212 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -1,21 +1,19 @@
-
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main { // 내리막길 + DFS + DP 근데 DP 어떻게 함-------------
+public class Main { //내리막길
     static int M, N;
     static int[][] box;
     static int[][] dp;
+    static int[][] way = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-
         box = new int[M][N];
         dp = new int[M][N];
         for (int m = 0; m < M; m++) {
@@ -25,32 +23,28 @@ public class Main { // 내리막길 + DFS + DP 근데 DP 어떻게 함----------
                 dp[m][n] = -1;
             }
         }
-
         System.out.println(dfs(0, 0));
-
     }
 
-    static int[][] way = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     private static int dfs(int i, int j) {
-        if (i == M - 1 && j == N - 1) { // 끝까지 도달
-            return 1;
+        if (i == M - 1 && j == N - 1) {
+            return 1; //M-1, N-1까지 오는 방법의 수 +1
         }
         if (dp[i][j] != -1) { //이미 방문한 적이 있음
             return dp[i][j];
         }
-        dp[i][j] = 0; // 방문 처리
+        dp[i][j] = 0; // 방문처리
         for (int w = 0; w < 4; w++) {
             int nx = i + way[w][0];
             int ny = j + way[w][1];
             if (nx < 0 || nx >= M || ny < 0 || ny >= N) {
                 continue;
             }
-            if (box[i][j] > box[nx][ny]) {
-                dp[i][j] += dfs(nx, ny);
+            if (box[i][j] > box[nx][ny]) { 
+                dp[i][j] += dfs(nx, ny); //...!
             }
         }
         return dp[i][j];
     }
-
 }
