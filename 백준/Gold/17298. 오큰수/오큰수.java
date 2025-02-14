b867cfc42958268d31d8ee0b58d6061ff1e647ac
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2025-02-14 15:05:10 +0900

    [Gold IV] Title: 오큰수, Time: 888 ms, Memory: 156536 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,56 +1,41 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
+	static int N;
+	static int[] arr;
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Stack<Integer> stack = new Stack<Integer>();
+		int[] ans = new int[N];
+		Stack<Integer> stack = new Stack<>();
 
-        int N = Integer.parseInt(br.readLine());
-        int[] seq = new int[N];
+		for (int i = 0; i < N; i++) {
+			while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+				ans[stack.pop()] = arr[i];
+			}
+			stack.push(i);
+		}
+		while (!stack.isEmpty()) {
+			ans[stack.pop()] = -1;
+		}
 
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			sb.append(ans[i]).append(" ");
+		}
+		System.out.println(sb.toString().trim());
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	}
 
-        for(int i = 0; i < N; i++) {
-            seq[i] = Integer.parseInt(st.nextToken());
-        }
-
-
-        for(int i = 0; i < N; i++) {
-
-            /*
-             * 스택이 비어있지 않으면서
-             * 현재 원소가 스택의 맨 위 원소가 가리키는 원소보다 큰 경우
-             * 해당 조건을 만족할 때 까지 stack의 원소를 pop하면서
-             * 해당 인덱스의 값을 현재 원소로 바꿔준다.
-             */
-            while(!stack.isEmpty() && seq[stack.peek()] < seq[i]) {
-                seq[stack.pop()] = seq[i];
-            }
-
-            stack.push(i);
-        }
-
-        /*
-         * 스택의 모든 원소를 pop하면서 해당 인덱스의 value를
-         * -1로 초기화한다.
-         */
-        while(!stack.isEmpty()) {
-            seq[stack.pop()] = -1;
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < N; i++) {
-            sb.append(seq[i]).append(' ');
-        }
-
-        System.out.println(sb);
-    }
 }
