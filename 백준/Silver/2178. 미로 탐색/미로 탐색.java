8af542db02df32e688edf86d5cf1a24c26d311a4
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2025-02-18 20:28:47 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14556 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,55 +1,57 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
-    static int[][] miro;
+	static int N, M;
+	static int[][] box;
+	static int[][] way = { { -1, 0 }, { 1, 0 }, { 0, 1 }, { 0, -1 } };
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        miro = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < M; j++) {
-                miro[i][j] = str.charAt(j) - '0';
-            }
-        }
-        // bfs
-        bfs(0, 0);//n-1, m-1
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		box = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			String line = br.readLine();
+			for (int j = 0; j < M; j++) {
+				box[i][j] = line.charAt(j) - '0';
+			}
+		}
 
-    static int[][] way = new int[][]{{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+		int[][] dp = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			Arrays.fill(dp[i], Integer.MAX_VALUE);
+		}
+		Queue<int[]> queue = new ArrayDeque<>();
+		queue.add(new int[] { 0, 0 });
+		dp[0][0] = 1;
+		while (!queue.isEmpty()) {
+			int[] now = queue.poll();
 
-    private static void bfs(int i, int j) {
-        boolean[][] visited = new boolean[N][M];
+			for (int w = 0; w < 4; w++) {
+				int nx = now[0] + way[w][0];
+				int ny = now[1] + way[w][1];
 
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.add(new int[]{i, j, 1});
-        visited[i][j] = true;
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-            for (int w = 0; w < 4; w++) {
-                int nx = now[0] + way[w][0];
-                int ny = now[1] + way[w][1];
-                if (nx < 0 || nx >= N || ny < 0 || ny >= M || visited[nx][ny] || miro[nx][ny] == 0) {
-                    continue;
-                }
-                if (nx == N - 1 && ny == M - 1) {
-                    System.out.println(now[2] + 1);
-                    return;
-                }
-                visited[nx][ny] = true;
-                queue.add(new int[]{nx,ny,now[2]+1});
-            }
-        }
+				if (!inRange(nx, ny))
+					continue;
+				if (box[nx][ny] == 1 && dp[nx][ny] == Integer.MAX_VALUE) {
+					queue.add(new int[] { nx, ny });
+					dp[nx][ny] = dp[now[0]][now[1]] + 1;
+				}
+			}
+		}
+		System.out.println(dp[N - 1][M - 1]);
 
-    }
+	}
+
+	static boolean inRange(int x, int y) {
+		return x >= 0 && x < N && y >= 0 && y < M;
+	}
 
 }
