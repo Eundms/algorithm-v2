071a5d2c2211438ae33834ec0e655497d0a8074f
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2025-02-25 09:52:18 +0900

    [Silver I] Title: 스타트와 링크, Time: 280 ms, Memory: 19768 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,54 +1,56 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
-    static int[][] box;
-    static boolean[] picked;
-    static int minValue = Integer.MAX_VALUE;
+	static int N; // 내림차순 순서, 어떤 순서로 서 있는지
+	static int[][] box;
+	static boolean[] visited;
+	static int minDiff = Integer.MAX_VALUE;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine()); // 배열의 크기
-        box = new int[N][N];
-        StringTokenizer st;
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                box[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        // N/2
-        picked = new boolean[N];
-        comb(0,0);
-        System.out.println(minValue);
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		box = new int[N][N];
 
-    static void comb(int cnt, int start) {
-        if (cnt == N / 2) {
-            int teamA = 0, teamB = 0;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    if (picked[i]==true && picked[j]==true) {
-                        teamA += box[i][j];
-                    } else if(picked[i]==false && picked[j]==false){
-                        teamB += box[i][j];
-                    }
-                }
-            }
-            minValue = Math.min(minValue, Math.abs(teamA - teamB));
-            return;
-        }
-        for (int i = start; i < N; i++) {
-            picked[i] = true;
-            comb(cnt + 1, i + 1);
-            picked[i] = false;
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				box[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		visited = new boolean[N];
+		perm(0, 0);
+		System.out.println(minDiff);
+	}
 
-        }
+	static void perm(int cnt, int start) {
+		if (cnt == N / 2) {
 
-    }
+			int teamA = 0, teamB = 0;
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (visited[i] && visited[j]) {
+						teamA += box[i][j];
+					}
+					if (!visited[i] && !visited[j]) {
+						teamB += box[i][j];
+
+					}
+				}
+			}
+
+			minDiff = Math.min(Math.abs(teamA - teamB), minDiff);
+			return;
+		}
+
+		for (int i = start; i < N; i++) {
+
+			visited[i] = true;
+			perm(cnt + 1, i + 1);
+			visited[i] = false;
+		}
+	}
 
 }
