9cb1cb0395a9fe8ab1567626e01b106f152c5880
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2025-03-06 09:52:53 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 5.27 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,62 +1,27 @@
 import java.util.*;
+
 class Solution {
-    static int JOB_SIZE;
-    public int solution(int[][] jobs) { // 요청_시각 작업_소요_시간
-        JOB_SIZE = jobs.length;
-        List<Item> arr = new ArrayList<>();
-        for(int i = 0; i < jobs.length; i++) {
-            arr.add(new Item(i, jobs[i][0], jobs[i][1]));
-        }
-        Collections.sort(arr, Comparator.comparingInt(x -> x.reqTime)); // 요청 빨리 온 순서대로 
+    public int solution(int[][] jobs) {
+        Arrays.sort(jobs, Comparator.comparingInt(x -> x[0])); // 요청 시각 순 정렬
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(x -> x[1])); // 소요 시간 짧은 순
         
-        PriorityQueue<Item> pq = new PriorityQueue<>();
-        int jIdx = 0;
-        int curTime = 0;
-        int sumReturnTime = 0;
+        int curTime = 0, sumReturnTime = 0, jIdx = 0, finishedCnt = 0, n = jobs.length;
         
-        int finishedCnt = 0;
-        while(true) {
-            if(finishedCnt == JOB_SIZE)break;
-            while(jIdx < JOB_SIZE && arr.get(jIdx).reqTime <= curTime) {
-                pq.add(arr.get(jIdx));
-                jIdx++;
+        while (finishedCnt < n) {
+            while (jIdx < n && jobs[jIdx][0] <= curTime) {
+                pq.add(jobs[jIdx++]); // 요청 시간이 현재 시간 이하인 작업 추가
             }
-      
-            if(!pq.isEmpty()){
-                Item cur = pq.poll();
-                int returnTime = cur.cost + curTime - cur.reqTime ;
-                sumReturnTime += returnTime;
-                curTime += cur.cost;     
-                finishedCnt += 1;
-            }else {
-                curTime += 1;
+            
+            if (!pq.isEmpty()) {
+                int[] curJob = pq.poll();
+                curTime += curJob[1]; // 소요 시간 추가
+                sumReturnTime += (curTime - curJob[0]); // 요청부터 완료까지 걸린 시간
+                finishedCnt++;
+            } else {
+                curTime = jobs[jIdx][0]; // 다음 요청 시간으로 건너뜀
             }
         }
-        return sumReturnTime/JOB_SIZE;
-    }
-    static class Item implements Comparable<Item>{
-        int id;
-        int reqTime;
-        int cost;
-        Item(int id, int reqTime, int cost) {
-            this.id = id;
-            this.reqTime = reqTime;
-            this.cost = cost;
-        }
-        @Override
-         public int compareTo(Item o) { 
-             if (this.cost == o.cost) {
-                 if (this.reqTime == o.reqTime) {
-                    return this.id - o.id;                     
-                 }
-                 return this.reqTime - o.reqTime;
-             }
-             return this.cost - o.cost;
-         }
         
-        @Override
-        public String toString() {
-            return this.id +" "+ this.reqTime+" "+ this.cost;
-        }
+        return sumReturnTime / n;
     }
 }
