ae0bdbf1154f346560ac3f8958323c71aeb5fd72
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2025-03-08 17:24:17 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 636 ms, Memory: 155176 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+		System.out.println(lisLength(arr));
+	}
+
+	public static int lisLength(int[] arr) {
+		List<Integer> lis = new ArrayList<>();
+
+		for (int num : arr) {
+			int pos = Collections.binarySearch(lis, num); // 값이 없다면, -(들어갈 위치 - 1) 반환
+			if (pos < 0)
+				pos = -(pos + 1); // 이분 탐색 위치 변환
+
+			if (pos == lis.size()) {
+				lis.add(num); // 새로 추가
+			} else {
+				lis.set(pos, num); // 값 교체
+			}
+		}
+		return lis.size(); // LIS 길이 반환
+	}
+}
