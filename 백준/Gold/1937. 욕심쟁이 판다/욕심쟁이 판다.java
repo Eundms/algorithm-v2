88967ea870b42f7908ec2a3c3d78fdd4dddc6965
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2025-03-11 09:31:13 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 492 ms, Memory: 36924 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -1,70 +1,61 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
 import java.util.StringTokenizer;
+
 public class Main {
 	static int N;
 	static int[][] box;
-	static int maxCnt;
-	//static boolean[][] visited;
 	static int[][] dp;
-	public static void main(String[] args) throws Exception{
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		box = new int[N][N];
-		
-		StringTokenizer st;
-		for(int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j =  0; j < N; j++) {
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
 				box[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
+
 		dp = new int[N][N];
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				//visited = new boolean[N][N];
-				//move(i,j, 1);
-				
-				maxCnt = Math.max(maxCnt,move(i,j));
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				dfs(i, j);
 			}
 		}
-		System.out.println(maxCnt);
-		
-	
+		int max = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				max = Math.max(max, dp[i][j]);
+			}
+		}
+		System.out.println(max);
 	}
-	static int[][] way = {{-1,0},{1,0},{0,-1},{0,1}};
-	private static int move(int i , int j) {
-		if(dp[i][j]!=0)return dp[i][j];
+
+	static int[][] way = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+
+	static int dfs(int i, int j) {
+		if (dp[i][j] != 0) {
+			return dp[i][j];
+		}
 		dp[i][j] = 1;
-		for(int w = 0; w < 4; w++) {
+		for (int w = 0; w < 4; w++) {
 			int nx = i + way[w][0];
 			int ny = j + way[w][1];
-			if(nx<0 || nx>=N || ny<0 || ny>=N)continue;
-			if(box[nx][ny]>box[i][j]) {
-				dp[i][j] = Math.max(dp[i][j],move(nx,ny)+1);
-			}
+			if (!inRange(nx, ny) || box[nx][ny] <= box[i][j])
+				continue;
+			dp[i][j] = Math.max(dp[i][j], dfs(nx, ny) + 1);
+
 		}
+
 		return dp[i][j];
 	}
-//	private static void move(int i, int j, int cnt) {
-//		if(visited[i][j])return;
-//		visited[i][j] = true;
-//		maxCnt = Math.max(maxCnt, cnt);
-//		for(int w = 0; w < 4; w++) {
-//			int nx = i + way[w][0];
-//			int ny = j + way[w][1];
-//			if(nx<0 || nx>=N || ny<0 || ny>=N)continue;
-//			if(box[nx][ny]>box[i][j]) {
-//				move(nx,ny, cnt+1);
-//			}
-//		}
-//		
-//	}
-	
+
+	static boolean inRange(int x, int y) {
+		return x >= 0 && x < N && y >= 0 && y < N;
+	}
 }
