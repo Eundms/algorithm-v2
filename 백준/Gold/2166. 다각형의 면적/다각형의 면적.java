82770c2a29d20b55aef63fcf8f1fa5ecf6199890
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2025-03-16 16:44:39 +0900

    [Gold V] Title: 다각형의 면적, Time: 176 ms, Memory: 20428 KB -BaekjoonHub

--- 백준/Gold/2166. 다각형의 면적/다각형의 면적.java
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.java
@@ -1,31 +1,38 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        
-        long[] x = new long[N + 1];
-        long[] y = new long[N + 1];
-        
-        for (int i = 0; i < N; i++) {
-            x[i] = sc.nextLong();
-            y[i] = sc.nextLong();
-        }
-        
-        x[N] = x[0];
-        y[N] = y[0];
+	static int N; // 전체 용액의 수
+	static long[][] items;
 
-        long sum1 = 0, sum2 = 0;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// 순서
+		N = Integer.parseInt(br.readLine()); // 전체 용액의 수
 
-        for (int i = 0; i < N; i++) {
-            sum1 += x[i] * y[i + 1];  // x_i * y_{i+1}
-            sum2 += y[i] * x[i + 1];  // y_i * x_{i+1}
-        }
+		items = new long[N + 1][2];
+		StringTokenizer st;
+		for (int n = 0; n < N; n++) {
+			st = new StringTokenizer(br.readLine());
+			items[n][0] = Long.parseLong(st.nextToken());
+			items[n][1] = Long.parseLong(st.nextToken());
+		}
+		items[N][0] = items[0][0];
+		items[N][1] = items[0][1];
+		long area = 0;
+		for (int i = 0; i < N; i++) {
+			long x1 = items[i][0];
+			long y1 = items[i][1];
 
-        double area = Math.abs(sum1 - sum2) / 2.0;
+			long x2 = items[i + 1][0];
+			long y2 = items[i + 1][1];
 
-        // 소수점 둘째 자리까지 출력
-        System.out.printf("%.1f\n", area);
-    }
+			area += x1 * y2 - x2 * y1;
+		}
+		System.out.println(String.format("%.1f", (double) (Math.abs(area) / 2.0)));
+	}
+
 }
