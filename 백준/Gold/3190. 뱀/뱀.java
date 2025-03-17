5ab8336552a9d82d3e3299c08da048c2c98efc01
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2025-03-17 14:02:52 +0900

    [Gold IV] Title: 뱀, Time: 108 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -1,131 +1,111 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Queue;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
-public class Main {
-    /**
-     * 몸길이를 늘려 머리를 다음칸에 위치 이동한 칸에 사과 O -> 사과 없어짐 + 꼬리 움직이지 않음 사과 X -> 꼬리위치 칸 비워줌
-     */
-    static int N; // 보드의 크기
-    static int[][] board; // 사과 1, 뱀 2, 그외 0
-    static int K; // 사과 갯수
-    static int L; // 뱀의 방향 변환 횟수
-    static int[][] way = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; //오른쪽-> 아래 -> 왼쪽 -> 위쪽
-    static Queue<int[]> bam ;
-    // L(왼쪽) : w-1
-    // D(오른쪽) : w+1
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine()); // 보드의 크기
-        K = Integer.parseInt(br.readLine()); // 사과의 개수
+class Main {
+	static int N;
+	static int[][] box;
+	static int K;
 
-        StringTokenizer st;
-        board = new int[N][N];
-        for (int k = 0; k < K; k++) {// 사과 위치
-            st = new StringTokenizer(br.readLine());
-            int i = Integer.parseInt(st.nextToken()) - 1;
-            int j = Integer.parseInt(st.nextToken()) - 1;
-            board[i][j] = 1;
-        }
+	static int EMPTY = 0, SNAKE = 1, APPLE = 2;
+	static int L; // 뱀의 방향 변환 횟수
+	static Snake snake;
+	static int[][] way = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+	static Map<Integer, Integer> directions = new HashMap<>();
 
-        L = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine()); // 보드의 크기
+		box = new int[N][N];
+		K = Integer.parseInt(br.readLine()); // 사과의 개수
+		for (int k = 0; k < K; k++) { // 사과의 위치
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int r = Integer.parseInt(st.nextToken()) - 1; // 행
+			int c = Integer.parseInt(st.nextToken()) - 1; // 열
+			box[r][c] = APPLE;
+		}
+		L = Integer.parseInt(br.readLine()); // 뱀의 방향 변환 횟수
 
-        int bamX = 0, bamY = 0; // 뱀X, 뱀Y
-        bam = new ArrayDeque<>();
-        bam.offer(new int[]{bamX,bamY});
-        board[bamX][bamY] = 2;
+		for (int l = 0; l < L; l++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken()); // time 초 후에
+			int d = st.nextToken().charAt(0) == 'L' ? 3 : 1; // 어떤 방향으로 회전
+			int t = 0;
+			directions.put(x, d);
+		}
 
-        //print();
+		int time = 0;
+		snake = new Snake(0, 0, 0);
+		box[0][0] = SNAKE;
+		while (true) {
 
-        boolean endGame = false;
-        int time = 0;
-        int wIdx = 0;
-        for (int l = 0; l < L; l++) {
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken()); // x초후에
-            char rotate = st.nextToken().charAt(0); // 해당 방향으로 90도 회전 : L 또는 D
-            //System.out.println(x+" "+rotate+">> ");
+			// t초 후에 왼쪽 혹은 오른쪽 90도 방향을 회전시킨다
+			int nx = snake.x + way[snake.w][0];
+			int ny = snake.y + way[snake.w][1];
+			time += 1;
 
-            while (true) {
+			if (!inRange(nx, ny) || box[nx][ny] == SNAKE) { // 0. 벽이나 자기 자신의 몸과 부딪히는 경우
+				break;
+			}
 
-                // 다음 뱀 길이 X, 다음 뱀 길이 Y
-                int nextBamX = bamX + way[wIdx][0];
-                int nextBamY = bamY + way[wIdx][1];
-                if (nextBamX < 0 || nextBamX >= N || nextBamY < 0 || nextBamY >= N) { // 벽에 부딪히면 게임이 끝난다.
-                    endGame = true;
-                    break;
-                }
-                if (board[nextBamX][nextBamY] == 2) {// 자신의 몸과 부딪히면 게임이 끝난다.
-                    endGame = true;
-                    break;
-                }
-                // 이동 ===
-                // 머리가 이동한다.
-                bam.offer(new int[]{nextBamX,nextBamY});
+			snake.body.addFirst(new int[] { nx, ny });
 
-                // 꼬리 이동 ? ===
-                if(board[nextBamX][nextBamY]==0){//이동한 칸에 사과가 없다면
-                    int[] tail = bam.poll();
-                    board[tail[0]][tail[1]] = 0;
-                }
-                board[nextBamX][nextBamY] = 2;
+			if (box[nx][ny] != APPLE) { // 2. 사과가 없다면
+				int[] tail = snake.body.pollLast(); // 꼬리 칸 없애기
+				box[tail[0]][tail[1]] = EMPTY; // 그 칸의 사과가 없어짐
+			}
+			box[nx][ny] = SNAKE; // 1. 머리 추가
+			snake.x = nx;
+			snake.y = ny;
 
-                bamX = nextBamX;
-                bamY = nextBamY;
-                time += 1;
-                if (time == x) {
-                    wIdx = (rotate == 'L' ? wIdx - 1 + 4 : wIdx + 1) % 4;
-                    break;
-                }
-//                System.out.println("Time :"+time);
-//                printBam();
-//                print();
-            }
-            if(endGame){
-                break;
-            }
-        }
-        if(endGame==false){
-            while(true){
-                int nextBamX = bamX + way[wIdx][0];
-                int nextBamY = bamY + way[wIdx][1];
-                if (nextBamX < 0 || nextBamX >= N || nextBamY < 0 || nextBamY >= N) { // 벽에 부딪히면 게임이 끝난다.
-                    break;
-                }
-                if (board[nextBamX][nextBamY] == 2) {// 자신의 몸과 부딪히면 게임이 끝난다.
-                    break;
-                }
-                bamX = nextBamX;
-                bamY = nextBamY;
-                time+=1;
-            }
-        }
-        System.out.println(time+1);
-    }
+			// 방향 전환
+			if (directions.containsKey(time)) {
+				snake.w = (snake.w + directions.get(time)) % 4;
+			}
+		}
+		System.out.println(time);
 
-    private static void printBam() {
-        int[][] bamArray = bam.toArray(new int[bam.size()][2]);
-        for(int[] t : bamArray){
-            System.out.println(t[0]+" "+t[1]+", \t");
-        }
-        System.out.println();
-    }
+	}
 
-    private static void print(){
-        for(int i=0;i<N;i++){
-            for(int j= 0; j < N; j++){
-                System.out.print(board[i][j]+" ");
-            }
-            System.out.println();
-        }
-        System.out.println();
+	static void printSnake() {
+		System.out.println(snake);
+	}
 
-    }
+	static void printBox() {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print(box[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+
+	static boolean inRange(int x, int y) {
+		return x >= 0 && x < N && y >= 0 && y < N;
+	}
+
+	static class Snake {
+		int x, y, w;
+		Deque<int[]> body;
+
+		Snake(int x, int y, int w) {
+			this.x = x;
+			this.y = y;
+			this.w = w;
+			this.body = new ArrayDeque<>();
+			this.body.add(new int[] { x, y });
+		}
+
+		@Override
+		public String toString() {
+			return "[" + this.x + "," + this.y + " " + this.w + " >> " + this.body + "]";
+		}
+	}
+
 }
