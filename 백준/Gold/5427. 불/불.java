3a0a7d13f2773f4277f4d6f403f183a6c877f16c
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2025-03-20 15:29:25 +0900

    [Gold IV] Title: 불, Time: 688 ms, Memory: 118756 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.java
+++ 백준/Gold/5427. 불/불.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
@@ -10,23 +11,22 @@ import java.util.StringTokenizer;
 public class Main {
 	static int T;
 	static char[][] box;
-	static int W, H;
+	static int W, H; // 빌딩 지도 너비와 높이
 	static char WALL = '#', EMPTY = '.', FIRE = '*';
 	static int sx, sy;
 	static int[][] way = { { -1, 0 }, { 1, 0 }, { 0, 1 }, { 0, -1 } };
 	static List<int[]> fires;
 
+	// https://www.acmicpc.net/problem/5427
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		T = Integer.parseInt(br.readLine());
-
 		for (int t = 0; t < T; t++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			W = Integer.parseInt(st.nextToken());
 			H = Integer.parseInt(st.nextToken());
 			fires = new ArrayList<>();
 			box = new char[H][W];
-
 			for (int h = 0; h < H; h++) {
 				String str = br.readLine();
 				for (int w = 0; w < W; w++) {
@@ -47,9 +47,9 @@ public class Main {
 			}
 
 			Queue<int[]> spread = new ArrayDeque<>();
-			for (int[] fire : fires) {
-				spread.add(new int[] { fire[0], fire[1], 0 });
-				fireTime[fire[0]][fire[1]] = 0;
+			for (int f = 0; f < fires.size(); f++) {
+				spread.add(new int[] { fires.get(f)[0], fires.get(f)[1], 0 }); // 1초후부터 확산
+				fireTime[fires.get(f)[0]][fires.get(f)[1]] = 0;
 			}
 
 			while (!spread.isEmpty()) {
@@ -58,44 +58,59 @@ public class Main {
 				for (int w = 0; w < 4; w++) {
 					int nx = now[0] + way[w][0];
 					int ny = now[1] + way[w][1];
-
-					if (!inRange(nx, ny) || fireTime[nx][ny] != Long.MAX_VALUE || box[nx][ny] != EMPTY) {
+					if (!inRange(nx, ny))
 						continue;
-					}
 
-					fireTime[nx][ny] = now[2] + 1;
-					spread.add(new int[] { nx, ny, now[2] + 1 });
+					if (fireTime[nx][ny] == Long.MAX_VALUE && box[nx][ny] == EMPTY) {
+						spread.add(new int[] { nx, ny, now[2] + 1 });
+						fireTime[nx][ny] = now[2] + 1;
+					}
 				}
 			}
+			// printFireTime(fireTime);
 
-			long result = movePerson(fireTime);
-			System.out.println(result == Long.MAX_VALUE ? "IMPOSSIBLE" : result);
+			long time = movePerson(fireTime);
+			if (time == Long.MAX_VALUE) {
+
+				System.out.println("IMPOSSIBLE");
+			} else {
+				System.out.println(time);
+			}
 		}
+
+	}
+
+	static void printFireTime(int[][] fireTime) {
+		for (int i = 0; i < H; i++) {
+			for (int j = 0; j < W; j++) {
+				System.out.print((fireTime[i][j] == Integer.MAX_VALUE ? -1 : fireTime[i][j]) + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
 	}
 
 	static long movePerson(long[][] fireTime) {
-		Queue<int[]> move = new ArrayDeque<>();
 		boolean[][] visited = new boolean[H][W];
-		move.add(new int[] { sx, sy, 0 });
+		Queue<int[]> move = new ArrayDeque<>();
+		move.add(new int[] { sx, sy, 1 });
 		visited[sx][sy] = true;
 
 		while (!move.isEmpty()) {
 			int[] now = move.poll();
-			int x = now[0], y = now[1], time = now[2];
-
 			for (int w = 0; w < 4; w++) {
-				int nx = x + way[w][0];
-				int ny = y + way[w][1];
-
-				if (!inRange(nx, ny))
-					return time + 1;
-				if (visited[nx][ny] || box[nx][ny] != EMPTY)
+				int nx = now[0] + way[w][0];
+				int ny = now[1] + way[w][1];
+				if (!inRange(nx, ny)) {
+					return now[2];
+				}
+				if (visited[nx][ny]) {
 					continue;
-				if (fireTime[nx][ny] <= time + 1)
-					continue;
-
-				visited[nx][ny] = true;
-				move.add(new int[] { nx, ny, time + 1 });
+				}
+				if (box[nx][ny] == EMPTY && fireTime[nx][ny] > now[2]) { // 불이 옮겨진 칸, 이제 붙으려는 칸으로 이동 불가능
+					visited[nx][ny] = true;
+					move.add(new int[] { nx, ny, now[2] + 1 });
+				}
 			}
 		}
 		return Long.MAX_VALUE;
@@ -104,4 +119,5 @@ public class Main {
 	static boolean inRange(int x, int y) {
 		return x >= 0 && x < H && y >= 0 && y < W;
 	}
+
 }
