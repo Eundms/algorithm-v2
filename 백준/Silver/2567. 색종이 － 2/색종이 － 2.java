7df7b6e74e45d8281e82b302ec744dec13060ef3
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2025-03-21 10:31:50 +0900

    [Silver IV] Title: 색종이 - 2, Time: 100 ms, Memory: 14052 KB -BaekjoonHub

--- 백준/Silver/2567. 색종이 － 2/색종이 － 2.java
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.java
@@ -1,68 +1,41 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int totalCnt;
-    static int MAX_LEN = 100;
-    static int[][] box;
-    static int blackN; // 검은 스카프의 수
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));//검은 스카프의 수
+	static int N;
+	static int[][] box = new int[102][102];
+	static int[][] way = { { -1, 0 }, { 1, 0 }, { 0, 1 }, { 0, -1 } };
 
-        totalCnt=0;
-        blackN = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
 
-        box = new int[MAX_LEN+2][MAX_LEN+2]; // 양의 정수로 주어짐 즉, 1,1 부터 100,100까지만 주어짐
-        StringTokenizer st;
-        for(int n=0;n<blackN;n++) {
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            for(int i = x; i < Math.min(x+10, MAX_LEN); i++) {
-                for(int j= y; j < Math.min(y+10, MAX_LEN); j++) {
-                    box[i][j] = 1;
-                }
-            }
-        }
+		for (int n = 0; n < N; n++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			for (int r = a; r < a + 10; r++) {
+				for (int c = b; c < b + 10; c++) {
+					box[r][c] = 1;
+				}
+			}
+		}
 
-        for(int i=0;i<MAX_LEN+1;i++) {
-            for(int j=0;j<MAX_LEN+1;j++) {
-                if(box[i][j]==1) {
-                    meltBlack(i,j); // 안쪽 둘레에 표시
-                }
-            }
-        }
-        //printMatrix();
-        System.out.println(totalCnt);
-
-    }
-
-    static int[][] way = new int[][] {{1,0},{-1,0},{0,1},{0,-1}}; //상하좌우 방향
-
-    private static void meltBlack(int i, int j) {
-        int[] now = new int[]{i,j};
-        for(int w=0;w<4;w++) {
-            int nx = now[0]+way[w][0];
-            int ny = now[1]+way[w][1];
-
-            if(nx<0 || nx > MAX_LEN || ny<0 || ny>MAX_LEN) {continue;} //범위 상황봐서 바꿔야 함
-            if(box[nx][ny]==0) { //다음으로 갈 곳이 흰색
-                totalCnt+=1;
-                box[now[0]][now[1]] = 2;
-            }
-        }
-    }
-
-    private static void printMatrix() { // 디버깅용 함수
-        for(int i=0;i<100;i++) {
-            for(int j=0;j<100;j++) {
-                System.out.print(box[i][j]+" ");
-            }
-            System.out.println();
-        }
-    }
+		int res = 0;
+		for (int i = 0; i <= 100; i++) {
+			for (int j = 0; j <= 100; j++) {
+				if (box[i][j] == 1) {
+					for (int d = 0; d < 4; d++) {
+						int nx = i + way[d][0];
+						int ny = j + way[d][1];
+						if (box[nx][ny] == 0) {
+							res++;
+						}
+					}
+				}
+			}
+		}
+		System.out.println(res);
+	}
 }
