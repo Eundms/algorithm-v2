71c624b62ca26728e37a3a16ad11b1148d9d5a46
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2025-05-23 09:53:12 +0900

    [level 2] Title: 튜플, Time: 61.02 ms, Memory: 99.1 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,30 +1,30 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(String s) {
-        s = s.substring(2, s.length() - 2); // 앞 "{{", 뒤 "}}" 제거
-        String[] groups = s.split("\\},\\{"); // "},{" 기준으로 나누기
-
+        int[] answer = {};
+        // 앞 {{ 제거, 뒤 }} 제거
+        // },{
+        s = s.substring(2, s.length() - 2);
+        String[] groups = s.split("\\},\\{");
+        
         List<String[]> all = new ArrayList<>();
-        for (String group : groups) {
+        for(String group : groups) {
             all.add(group.split(","));
         }
-
-        all.sort(Comparator.comparingInt(a -> a.length));
-
-        Set<Integer> seen = new HashSet<>();
+        all.sort(Comparator.comparingInt(x -> x.length));
+        
         List<Integer> result = new ArrayList<>();
-
-        for (String[] group : all) {
-            for (String numStr : group) {
-                int num = Integer.parseInt(numStr);
-                if (!seen.contains(num)) {
+        Set<Integer> seen = new HashSet<>();
+        for(String[] group :  all){
+            for(String item : group){
+                int num = Integer.parseInt(item);
+                if(!seen.contains(num)){
                     seen.add(num);
                     result.add(num);
                 }
             }
         }
-
+  
         return result.stream().mapToInt(i -> i).toArray();
     }
 }
