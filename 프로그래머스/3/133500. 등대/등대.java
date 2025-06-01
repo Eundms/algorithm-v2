bc012d627d4ae1dd78c3693193a6ce55655363e8
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2025-06-01 09:57:05 +0900

    [level 3] Title: 등대, Time: 95.55 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/3/133500. 등대/등대.java
+++ 프로그래머스/3/133500. 등대/등대.java
@@ -1,34 +1,39 @@
 import java.util.*;
 class Solution {
     static int answer = 0;
-    static List<Integer>[] adj;
-    public int solution(int n, int[][] lighthouse) throws Exception{
+    static List<Integer> adj[];
+    public int solution(int n, int[][] lighthouse) {
         
         adj = new ArrayList[n+1];
-        for(int i = 0; i < n+1; i++) {
+        for(int i = 0 ; i < n+1; i++) {
             adj[i] = new ArrayList<>();
         }
         
-        for(int i = 0; i < n-1; i++) {
+        for(int i = 0; i < n - 1; i++) {
             adj[lighthouse[i][0]].add(lighthouse[i][1]);
             adj[lighthouse[i][1]].add(lighthouse[i][0]);
         }
         
         dfs(1, 0);
+        
         return answer;
     }
-    private int dfs(int node, int prev) {
-        if(adj[node].size() == 1 && adj[node].get(0) == prev) {  // 리프노드인 경우, 불을 켜지 않는다
-            return 1;
+    int dfs(int node, int prev) {
+        // 리프노드 -> 안켜야 함,  리프노드 : adj에 부모만 있음. 그리고 adj에 있는 그 노드가 이전값
+        if(adj[node].size() == 1 && adj[node].get(0) == prev) {
+            return 1; // 상위 누군가는 켜야 함
         }
         int net = 0;
-        for(int i = 0; i < adj[node].size(); i++) {
-            int next = adj[node].get(i);
-            if(next == prev)continue;
+        for(int next : adj[node])  {
+            if(next == prev) {continue;} // 자식 <-> 부모 무한 루프 제거 
             net += dfs(next, node);
         }
-        if(net == 0)return 1;
-        answer += 1;
-        return 0;
+        if(net == 0) { // 안켜도 됨
+            return 1;  // 상위 누군가는 켜야함
+        }
+        answer += 1; // 본인 켜야 함 
+        return 0;  // 상위는 안켜도 됨 
+        
     }
+    
 }
