97bb43d6a1d1056f81efbb62f87af0cc5379e763
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2025-06-22 17:14:21 +0900

    [Gold V] Title: 0 만들기, Time: 160 ms, Memory: 17132 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.java
+++ 백준/Gold/7490. 0 만들기/0 만들기.java
@@ -3,58 +3,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+	static int T, N;
+	static char[] oper = new char[9];
+	static StringBuilder sb = new StringBuilder();
 
-    static int T;
-    static int N;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
+		while (T-- > 0) {
+			N = Integer.parseInt(br.readLine());
+			search("1", 1, 0, 1, 1);
+			sb.append('\n');
+		}
+		System.out.println(sb);
+	}
 
-    public static void main(String[] args) throws IOException { // 행운의 문자열
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        T = Integer.parseInt(br.readLine()); // xptmxm zpdltmdml rotn
-        for (int test = 1; test <= T; test++) {
-            N = Integer.parseInt(br.readLine());
-            back("1", 2);
-            System.out.println();
-        }
+	static void search(String str, int depth, int sum, int num, int op) {
+		if (depth == N) {
+			if (sum + (num * op) == 0) {
 
-    }
+				sb.append(str + '\n');
+			}
+			return;
+		}
+		search(str + " " + (depth + 1), depth + 1, sum, (num * 10) + depth + 1, op);
+		search(str + "+" + (depth + 1), depth + 1, sum + (num * op), depth + 1, 1);
+		search(str + "-" + (depth + 1), depth + 1, sum + (num * op), depth + 1, -1);
+	}
 
-    private static void back(String str, int num) {
-        if (num == N + 1) {
-            int sum = 0;
-            int number = 0;
-            char sign = '+';
-
-            String nStr = str.replace(" ", "");
-            for (int i = 0; i < nStr.length(); i++) {
-                if (nStr.charAt(i) >= '0' && nStr.charAt(i) <= '9') {
-                    number = number * 10 + (nStr.charAt(i) - '0');
-                    if (i == nStr.length() - 1) {
-                        if (sign == '+') {
-                            sum += number;
-                        } else {
-                            sum -= number;
-                        }
-                    }
-                } else {
-                    char item = nStr.charAt(i);
-                    if (sign == '+') {
-                        sum += number;
-                    } else if (sign == '-') {
-                        sum -= number;
-                    }
-                    number = 0;
-                    sign = item;
-                }
-
-            }
-            if (sum == 0) {
-                System.out.println(str);
-            }
-            return;
-        }
-        back(str + " " + num, num + 1);
-        back(str + "+" + num, num + 1);
-        back(str + "-" + num, num + 1);
-
-    }
 }
