cff55c742e5d34840dc0c93e7f3e29ef124d4bbe
EunJeongPark <50352139+Eundms@users.noreply.github.com>
2025-06-29 12:23:17 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 6.33 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -1,48 +1,40 @@
 import java.util.*;
 class Solution {
-    // 0, 0 에서 N-1, N-1 이동 최소 비용
-    // 방향 전환 (=코너) : 500
-    // 총 경로 - 1 : 100
-    static int N, M;
-    static int[][] way = {  {0,1}, {1,0}, {-1,0}, {0,-1}}; // 0 1 2 3
-    public int solution(int[][] board) {
-        int answer = Integer.MAX_VALUE;
-        N = board.length; 
-        M = board[0].length;
-        
-        int[][][] cost = new int[N][M][4];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
+    static int N;
+    static int[][] way = {{0,1},{1,0},{0,-1},{-1,0}};
+    static int[][][] cost;
+    public int solution(int[][] box) {
+        int answer = 0;
+        N = box.length;
+        cost = new int[N][N][4];
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
                 Arrays.fill(cost[i][j], Integer.MAX_VALUE);
             }
         }
-        
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.add(new int[]{0, 0, 0,  -1}); // 0,0 좌표 | 지금까지 cost | 방향 
-        
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-            if(now[0] == N-1 && now[1] == M-1) {
-                answer = Math.min(answer, now[2]);
-                continue;
+        // x , y, 방향, 비용
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparing(x -> x[3]));
+        pq.add(new int[]{0, 0, -1, 0 });
+        while(!pq.isEmpty()){
+            int[] now = pq.poll();
+            if(now[0] == N-1 && now[1] == N-1){
+                return now[3];
             }
-            for (int w = 0; w < 4; w++) {
-                int nx = now[0] + way[w][0];
+            for(int w = 0; w < 4; w ++){
+                int nx  = now[0] + way[w][0];
                 int ny = now[1] + way[w][1];
-                if(!inRange(nx, ny)) continue;
-                if(board[nx][ny] == 0){
-                    int nextCost = now[2] + (now[3] == -1 || now[3] == w || now[3] == 3 - w  ? 100  : 600);
-                    if(cost[nx][ny][w] > nextCost) {
-                        cost[nx][ny][w] = nextCost;
-                        queue.add(new int[]{nx, ny, nextCost, w});
-                        
-                    }
+                if(!inRange(nx, ny) || box[nx][ny] == 1)continue;
+                int next = now[3] + ((now[2] == -1 || now[2] == w) ? 100 : 600);
+                if(cost[nx][ny][w] > next) {
+                    cost[nx][ny][w] = next;
+                    pq.add(new int[]{nx, ny, w, next});
                 }
             }
         }
-        return answer;
+        return 0;
     }
-    static boolean inRange(int x, int y) {
-        return x >= 0 && x < N && y >= 0 && y < M;
+        static boolean inRange(int x, int y) {
+        return x >= 0 && x < N && y >= 0 && y < N;
     }
+
 }
